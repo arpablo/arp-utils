deeed65bf1b9cbb253a2d428cf8373a83fbf73a5
Armin Pfarr <apfarr@icloud.com>
2017-02-14 08:15:42 +0100

    Testclasses are now using Saxon-HE Transformer

--- src/test/java/de/arp/utils/xml/XMLTests.java
+++ src/test/java/de/arp/utils/xml/XMLTests.java
@@ -22,7 +22,7 @@ public class XMLTests {
 	@Test
 	public void testIdentityTransformer() {
 		Source src = new StreamSource(getClass().getResourceAsStream("/test1.xml"));
-		ChainedFilterTransformer t = new ChainedFilterTransformer();
+		ChainedFilterTransformer t = ChainedFilterTransformer.newSaxon_HETransformer();
 		try {
 			t.transform(src,  new StreamResult(System.out));
 		} catch (Exception ex) {
@@ -33,7 +33,7 @@ public class XMLTests {
 	@Test
 	public void testStyleTransformer() {
 		Source src = new StreamSource(getClass().getResourceAsStream("/test1.xml"));
-		ChainedFilterTransformer t = new ChainedFilterTransformer();
+		ChainedFilterTransformer t = ChainedFilterTransformer.newSaxon_HETransformer();
 		try {
 			Source style = new StreamSource(getClass().getResourceAsStream("/skipTest1.xsl"));
 			t.addStyle(style);
@@ -46,7 +46,7 @@ public class XMLTests {
 	@Test
 	public void testUppercaseFilter() {
 		Source src = new StreamSource(getClass().getResourceAsStream("/test1.xml"));
-		ChainedFilterTransformer t = new ChainedFilterTransformer();
+		ChainedFilterTransformer t = ChainedFilterTransformer.newSaxon_HETransformer();
 		t.addFilter(new ElementNamesToUpperCaseFilter());
 		try {
 			t.transform(src,  new StreamResult(System.out));
@@ -58,7 +58,7 @@ public class XMLTests {
 	@Test
 	public void testUppercaseLowercaseFilter() {
 		Source src = new StreamSource(getClass().getResourceAsStream("/test1.xml"));
-		ChainedFilterTransformer t = new ChainedFilterTransformer();
+		ChainedFilterTransformer t = ChainedFilterTransformer.newSaxon_HETransformer();
 		t.addFilter(new ElementNamesToUpperCaseFilter());
 		t.addFilter(new ElementNamesToLowerCaseFilter());
 		try {
@@ -71,7 +71,7 @@ public class XMLTests {
 	@Test
 	public void testUppercaseLowercaseStyleFilter() {
 		Source src = new StreamSource(getClass().getResourceAsStream("/test1.xml"));
-		ChainedFilterTransformer t = new ChainedFilterTransformer();
+		ChainedFilterTransformer t = ChainedFilterTransformer.newSaxon_HETransformer();
 		t.addFilter(new ElementNamesToUpperCaseFilter());
 		t.addFilter(new ElementNamesToLowerCaseFilter());
 		try {
